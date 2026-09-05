e0560547adb993533e997694863b1ba78110f818
Varycat <alvaro.pion@gmail.com>
2026-09-05 02:07:45 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='bible-study-library-v5';
-const FILES=['./','index.html','css/app.css','js/app.js','data/studies.json','data/topics.json','data/verses.json','manifest.webmanifest','icons/icon-180.png','icons/icon-192.png','icons/icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('index.html')));return;}e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
